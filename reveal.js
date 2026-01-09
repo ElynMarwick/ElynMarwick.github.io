12e91368162bec901867ee95395ae10085ffdbb6
Elyn Marwick <elynmarwick@Elyns-MacBook-Air.local>
2026-01-09 12:42:58 +1100

    Reveal Animation added for contact page

--- reveal.js
+++ reveal.js
@@ -13,12 +13,3 @@ window.addEventListener("load", () => {
         }
     });        
 });
-/*
-window.addEventListener("load", () => {
-    const reveals= document.querySelectorAll (".reveal");
-    reveals.forEach((reveal, index) => {
-        reveal.style.transitionDelay = `${index * 0.4}s`;
-        reveal.classList.add("active");
-    });        
-});
-*/
